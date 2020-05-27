0f2064843f6495b6a4f4d030d3608fc616ded38f
kamik4ze <41435231+kamik4ze@users.noreply.github.com>
2020-05-27 10:07:53 +0200

    users

---
+++ src/Entity/User.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ */
+class User {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    private $pass;
+
+    public function getUsername(): string {
+        return $this->name;
+    }
+
+    public function setUsername(string $name): void {
+        $this->name = $name;
+    }
+
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function setId(int $id): void {
+        $this->id = $id;
+    }
+
+    public function getPass(): string {
+        return $this->pass;
+    }
+
+    public function setPass(string $pass): void {
+        $this->pass = $pass;
+    }
+
+}
